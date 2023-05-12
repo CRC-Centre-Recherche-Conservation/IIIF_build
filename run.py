b32ae5c30446aff05c80b4cdac73a79e26ef46df
rayondemiel <maximehumeau44@gmail.com>
2023-05-12 18:10:06 +0200

    plan

---
+++ run.py
@@ -0,0 +1,11 @@
+
+
+# https://gitlab.huma-num.fr/jpressac/niiif-niiif
+
+# plan
+
+#recuperer manifest existant + metadonnées
+# garder seulement les images interessantes
+# ajouter les annotations
+# ajouter autres collections d'images pour faire des calques
+#donc besoin d'upload sur nakala -> pour image iiif 
