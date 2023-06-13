7d3cbfbd7c428ec6009bc5189c3cb4f55f8eeb11
rayondemiel <maximehumeau44@gmail.com>
2023-06-13 18:02:37 +0200

    build canvas with image and services

--- run.py
+++ run.py
@@ -3,13 +3,13 @@ import os
 import http.server
 import socketserver
 import click
-from iiif_prezi3 import AnnotationPage, Canvas, ResourceItem
-
+from iiif_prezi3 import Canvas, ResourceItem, AnnotationPage, Annotation
 
 from src.data import DataAnnotations
 from src.iiif import AnnotationIIIF, ManifestIIIF
 from src.opt.data_variables import LANGUAGES
 from src.opt.variables import URI_CRC
+
 
 # https://gitlab.huma-num.fr/jpressac/niiif-niiif
 
@@ -21,9 +21,14 @@ from src.opt.variables import URI_CRC
 # ajouter autres collections d'images pour faire des calques
 # donc besoin d'upload sur nakala -> pour image iiif
 
+# pour l'intant, ne lit que les API Presentation 2.0
+
+# to check https://github.com/IIIF/presentation-validator
+
 @click.group()
 def run_manifest():
     pass
+
 
 @run_manifest.command()
 @click.option("--config", "config", type=click.Path(exists=True, dir_okay=False, file_okay=True),
@@ -34,6 +39,12 @@ def run_manifest():
                                                                           you need to inquire the url : https://data.crc.fr/iiif/. The path manifests is automaticaly adding by the script.")
 @click.option("--usage-memory", "usage_memory", type=bool, default=False, is_flag=True, help="To see usage of RAM")
 def build_manifest(*args, **kwargs):
+    """
+    Build manifest IIIF with standard API Presentation 3.0
+    :param args:
+    :param kwargs:
+    :return: Manifest API Presentation 3.0
+    """
     data = DataAnnotations("data/ms59_annotation_iiif.csv", delimiter=";")
 
     # build manifest
@@ -51,47 +62,49 @@ def build_manifest(*args, **kwargs):
         manifest.annotation[uri] = list_anno
 
     for n_canvas, uri_canvas in enumerate(manifest.canvases):
+        #get original data
         canvas = manifest.canvases[uri_canvas]
 
-        """canvas_img = Canvas(id=canvas['@id'],
+        # Canvas entities
+        canvas_img = Canvas(id=canvas['@id'],
                             width=canvas['width'],
-                            height=canvas['height']) #peut etre passer par item ressource
+                            height=canvas['height'])
 
-        print(canvas['images'][0]['resource']['width'])
-
+        # Ressource image entities for canvas
         ressource_img = ResourceItem(id=canvas['images'][0]['resource']['@id'],
                                      type=canvas['images'][0]['resource']['@type'],
                                      format=canvas['images'][0]['resource']['format'],
                                      height=canvas['images'][0]['resource']['height'],
                                      width=canvas['images'][0]['resource']['width'])
 
-        anno_page = AnnotationPage(id=URI_CRC+f"/page/p{str(n_canvas)}/1")"""
-        canvas_img = manifest.manifest.make_canvas(id=canvas['@id'], width=canvas['width'], height=canvas['height'])
-        canvas_img.add_image(
-            image_url=canvas['images'][0]['resource']['@id'],
-            anno_page_id="https://iiif.io/api/cookbook/recipe/0001-mvm-image/page/p1/1",
-            anno_id="https://iiif.io/api/cookbook/recipe/0001-mvm-image/annotation/p0001-image",
-            format=canvas['images'][0]['resource']['format'],
-            height=canvas['images'][0]['resource']['height'],
-            width=canvas['images'][0]['resource']['width']
-            )
+        # Annotation for add ressource image in canvas
+        anno_img = Annotation(id=URI_CRC + f"/annotation/p{str(n_canvas)}-image",
+                              motivation="painting",
+                              body=ressource_img,
+                              target=canvas_img.id)
+
+        #Add service to image
+        anno_img.make_service(id=canvas['images'][0]['resource']['service']['@id'],
+                              type="ImageService3",
+                              profile="level2")
+
+        #Page annotation for canvas
+        anno_page = AnnotationPage(id=URI_CRC + f"/page/p{str(n_canvas)}/1")
+        anno_page.add_item(anno_img)
+
 
         for n_anno, data_anno in enumerate(manifest.annotation[uri_canvas]):
             AnnotationIIIF(canvas=canvas, data=data_anno, uri=uri_canvas, n=(n_canvas, n_anno), **kwargs)
-        #canvas_img.add_item(ressource_img)
-        #manifest.manifest.add_item(canvas_img)
 
-
+        #Add annotation by canvas
+        canvas_img.add_item(anno_page)
+        #Add cavas in manifest
+        manifest.manifest.add_item(canvas_img)
 
     if kwargs['usage_memory']:
         print("The size of the manifest.canvases is:", sys.getsizeof(manifest.canvases), "bytes.")
         print("The size of the annotation is:", sys.getsizeof(manifest.annotation), "bytes.")
         print("The size of the manifest is:", sys.getsizeof(manifest.manifest), "bytes.")
-
-
-
-
-
 
     manifest.build_thumbnail()
     print(manifest._print_json())
@@ -110,8 +123,9 @@ def server_manifest():
 
         def do_HEAD(self):
             self._set_headers()
+
         def do_GET(self):
-            #self._set_headers()
+            # self._set_headers()
 
             if self.path == '/':
                 self.path = 'output/'
@@ -129,7 +143,7 @@ def server_manifest():
     try:
         my_server = socketserver.TCPServer(("", PORT), handler_object)
     except OSError:
-        PORT = PORT+1
+        PORT = PORT + 1
         my_server = socketserver.TCPServer(("", PORT), handler_object)
     print('Listening on http://localhost:%s' % PORT)
     # Star the server
--- src/iiif.py
+++ src/iiif.py
@@ -2,9 +2,9 @@ import pandas as pd
 import requests
 import yaml
 from yaml.loader import SafeLoader
-from iiif_prezi3 import Manifest, KeyValueString, config, ExternalItem, ResourceItem, Annotation
+from iiif_prezi3 import Manifest, KeyValueString, config, ExternalItem, ResourceItem, Annotation, AnnotationPage
 
-from src.opt.variables import URI_CRC, SCHEME_DATA_ANNOTATION
+from src.opt.variables import URI_CRC
 from .forms import Rectangle, Marker
 
 
@@ -15,6 +15,7 @@ from .forms import Rectangle, Marker
 class IIIF(object):
     canvases = {}
     manifest = Manifest
+    annotation = {}
 
     def __init__(self, uri, **kwargs):
         self.uri = uri
@@ -36,7 +37,8 @@ class IIIF(object):
 class ManifestIIIF(IIIF):
     rights = ['Etalab 2.0', "https://spdx.org/licenses/etalab-2.0.html"]
     institution = [("Centre de la Recherche sur la Conservation", "https://crc.mnhn.fr/fr")]
-    attribution = KeyValueString(label="Attribution", value="<span>Centre de la Recherche sur la Conservation. <a href=\"https://spdx.org/licenses/etalab-2.0.html\">OPEN LICENCE 2.0</a> <a href=\"https://spdx.org/licenses/etalab-2.0.html\" title\"Etalab 2.0\"><img src=\"https://www.etalab.gouv.fr/wp-content/uploads/2011/10/licence-ouverte-open-licence.gif\"/></a></span>")
+    attribution = KeyValueString(label="Attribution",
+                                 value="<span>Centre de la Recherche sur la Conservation. <a href=\"https://spdx.org/licenses/etalab-2.0.html\">OPEN LICENCE 2.0</a> <a href=\"https://spdx.org/licenses/etalab-2.0.html\" title\"Etalab 2.0\"><img src=\"https://www.etalab.gouv.fr/wp-content/uploads/2011/10/licence-ouverte-open-licence.gif\"/></a></span>")
     description = 'None'
     metadata = []
     seealso = []
@@ -53,6 +55,8 @@ class ManifestIIIF(IIIF):
         return self.json['label'] + ", analyses physico-chimique @CRC"
 
     def get_metadata(self):
+        if self.verbose:
+            print("Getting metadata!")
         return self.json['metadata']
 
     def get_preconfig(self, filename):
@@ -81,7 +85,8 @@ class ManifestIIIF(IIIF):
                 if config_yaml['licence']['link'].startswith('http') and config_yaml['licence']['link'] is not None:
                     self.rights[1] = config_yaml['licence']['link']
                 else:
-                    raise ValueError("You need to indicate an URI of your licence. Need to start by http or https protocols")
+                    raise ValueError(
+                        "You need to indicate an URI of your licence. Need to start by http or https protocols")
                 if config_yaml['licence']['attribution'].upper() != 'NONE':
                     self.attribution = KeyValueString(label="Attribution", value=config_yaml['licence']['attribution'])
 
@@ -115,14 +120,21 @@ class ManifestIIIF(IIIF):
             # Authors
             for n, author in enumerate(config_yaml['authors']):
                 if n == 1:
-                    self.metadata.append(KeyValueString(label='Responsable Scientifique', value=author['name'].upper() + " " +
-                                                                                 author['forename'] if author['forename'].upper() != 'NONE' else 'n.c.' + ", " +
-                                                                                 author['role']if author['role'].upper() != 'NONE' else 'n.c.'))
+                    self.metadata.append(
+                        KeyValueString(label='Responsable Scientifique', value=author['name'].upper() + " " +
+                                                                               author['forename'] if author[
+                                                                                                         'forename'].upper() != 'NONE' else 'n.c.' + ", " +
+                                                                                                                                            author[
+                                                                                                                                                'role'] if
+                        author['role'].upper() != 'NONE' else 'n.c.'))
                 else:
                     self.metadata.append(
                         KeyValueString(label=f'Responsable Scientifique {str(n)}', value=author['name'].upper() + " " +
-                                                                               author['forename'] if author['forename'].upper() != 'NONE' else 'n.c.' + ", " +
-                                                                               author['role'] if author['role'].upper() != 'NONE' else 'n.c.'))
+                                                                                         author['forename'] if author[
+                                                                                                                   'forename'].upper() != 'NONE' else 'n.c.' + ", " +
+                                                                                                                                                      author[
+                                                                                                                                                          'role'] if
+                        author['role'].upper() != 'NONE' else 'n.c.'))
 
     def build_manifest(self):
 
@@ -137,7 +149,8 @@ class ManifestIIIF(IIIF):
             summary=self.description,
             metadata=self.metadata,
             requiredStatement=self.attribution,
-            seeAlso=[ExternalItem(id=self.json['@id'], format="application/ld+json", type="Manifest", label=self.json['label'])]
+            seeAlso=[ExternalItem(id=self.json['@id'], format="application/ld+json", type="Manifest",
+                                  label=self.json['label'])]
         )
 
     def build_thumbnail(self):
@@ -173,46 +186,63 @@ class SequenceIIIF(IIIF):
 
 
 class AnnotationIIIF:
-    resize = False
 
-    #self init:
-    #annotationPage = AnnotationPage(id=anno_page_id) -> creer un id pour la page en automatique
-    #ajouter l'annotation dans la page annotationPage.add_item(annotation)
-    #canvas.add_item(annotationPage)
-
-    #make_annotation()
-
-    def check_dimension(self, data_anno: SCHEME_DATA_ANNOTATION, canvas):
-        if data_anno['Dimensions']['width'] != canvas['images'][0]['resource']['width']:
-            self.resize = True
-        elif data_anno['Dimensions']['height'] != canvas['images'][0]['resource']['height']:
-            self.resize = True
+    def __init__(self, canvas: dict, data: dict, uri: str, n: tuple, **kwargs):
+        self.data = data
+        self.uri = uri
+        self.canvas = canvas
+        self.n = n
+        self.verbose = kwargs.get('verbose', False)
+    # ajouter l'annotation dans la page annotationPage.add_item(annotation)
+    # canvas.add_item(annotationPage)
 
     def make_annotation(self):
-        if self.resize:
-            pass
-        else:
-            pass
+        ResourceItem(id="http://iiif.io/api/presentation/2.1/example/fixtures/resources/page1-full.png",
+                     type="sc:Image",
+                     format="image/png",
+                     height=1800,
+                     width=1200)
+        return Annotation(
+            i
+        )
 
-
-    def make_forms(self, data_anno: SCHEME_DATA_ANNOTATION):
-        if isinstance(data_anno['Type'], str):
-            if data_anno['Type'].upper() == 'RECTANGLE':
-                pass
-            elif data_anno['Type'].upper() == 'MARKER':
-                pass
+    def _make_forms(self) -> str:
+        """
+        Function to get <svg> balise content for annotation iiif
+        :param json: manifest iiif serialized
+        :return: str, <svg> html
+        """
+        if isinstance(self.data['Type'], str):
+            if self.data['Type'].upper() == 'RECTANGLE':
+                rectangle = Rectangle(_id=self.data['Name'], image_url=self.uri, _type=self.data['Type_analysis'],
+                                      x=self.data['Coordinates']['x'], y=self.data['Coordinates']['y'],
+                                      w=self.data['Coordinates']['w'], h=self.data['Coordinates']['h'],
+                                      verbose=self.verbose)
+                rectangle.check_dim_manifest(self.canvas)
+                dimension = rectangle.dim_img_origin if rectangle.dim_img_origin is not None else rectangle.image_size
+                balise = rectangle.fit()
+            elif self.data['Type'].upper() == 'MARKER':
+                marker = Marker(_id=self.data['Name'], image_url=self.uri, _type=self.data['Type_analysis'],
+                                x=self.data['Coordinates']['x'], y=self.data['Coordinates']['y'], verbose=self.verbose)
+                marker.check_dim_manifest(self.canvas)
+                dimension = marker.dim_img_origin if marker.dim_img_origin is not None else marker.image_size
+                balise = marker.fit()
             else:
                 raise ValueError("The data annotation type need to be 'rectangle' or 'marker'")
+            return f"""
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="{str(dimension[0])}" height="{str(dimension[1])}" xmlns:xlink="http://www.w3.org/1999/xlink">
+            {balise}
+            </svg>
+            """
         else:
             raise TypeError('The data annotation type need to be string.')
 
-
     @classmethod
-    def data_annotation(cls, row: pd.DataFrame) -> SCHEME_DATA_ANNOTATION:
+    def data_annotation(cls, row: pd.DataFrame) -> dict:
         """
         Function to get data and build annotation object
         :param row:
-        :return:
+        :return: dict
         """
         try:
             tags = [tag.strip() for tag in row['Tags'].values[0].split(',')]
@@ -222,20 +252,20 @@ class AnnotationIIIF:
             pass
         try:
             return {
-                        'Name': row['Name'].values[0],
-                        'Type': row['Type'].values[0],
-                        'Tags': tags,
-                        'Dimensions': {
-                            'width': row['Dimensions'].values[0].split('x')[0].strip(),
-                            'height': row['Dimensions'].values[0].split('x')[1].strip(),
-                        },
-                        'Identifier': row['Identifier'].values[0],
-                        'Coordinates':
-                            {'x': row['X'].values[0], 'y': row['Y'].values[0], 'w': row['W'].values[0],
-                             'h': row['H'].values[0]},
-                        'Value': row['Value'].values[0],
-                        'URI': row['Reference.1'].values[0]
-                    }
+                'Name': row['Name'].values[0],
+                'Type': row['Type'].values[0],
+                'Type_analysis': row['Character'].values[0],
+                'Tags': tags,
+                'Dimensions': {
+                    'width': row['Dimensions'].values[0].split('x')[0].strip(),
+                    'height': row['Dimensions'].values[0].split('x')[1].strip(),
+                },
+                'Identifier': row['Identifier'].values[0],
+                'Coordinates':
+                    {'x': row['X'].values[0], 'y': row['Y'].values[0], 'w': row['W'].values[0],
+                     'h': row['H'].values[0]},
+                'Value': row['Value'].values[0],
+                'URI': row['Reference.1'].values[0]
+            }
         except Exception as err:
             print(err)
-
